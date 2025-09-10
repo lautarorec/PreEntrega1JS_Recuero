56ce2da93c5d4eeafa900924436e6fdfc78c7ce7
laucha <laucha@gmail.com>
2025-09-10 19:36:49 -0300

    agregues catalogos dentro de los prompt

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ class Mate {
                 this.color = color
                 }
         
-        mostrar_catalogo_mates(){
+        mostrar_catalogo_mates(){ //funcion que me devuelve el catalogo de mates en forma de lista
 
-            return "#" + this.id + "$" + this.precio + "-" + this.marca + this.color
+            return "#" + this.id + "-" + this.modelo + "-" + this.marca + "-" + this.color + "$" + this.precio
         }
 
-        getPrecio(){
+        getPrecio(){ //funcion que me devuelve el valor precui
             return this.precio
         }
 }
@@ -25,9 +25,9 @@ class Vaso {
                 this.color = color
                 }
         
-        mostrar_catalogo_vasos(){
+        mostrar_catalogo_vasos(){ //funcion que me devuelve el catalogo de vasos en forma de lista
 
-            return "#" + this.id + "$" + this.precio + "-" + this.marca + this.color
+            return "#" + this.id + "-" + this.modelo + "-" + this.marca + "-" + this.color + "$" + this.precio
         }
 
         getPrecio(){
@@ -43,7 +43,7 @@ class Termo{
                 this.color = color
                 }
         
-        mostrar_catalogo_termos(){
+        mostrar_catalogo_termos(){ //funcion que me devuelve el catalogo de termos en forma de lista
 
             return "#" + this.id + "-" + this.modelo + "-" + this.marca + "-" + this.color + "$" + this.precio
         }
@@ -53,7 +53,7 @@ class Termo{
         }
 }
 
-let arreglo_mates = []
+let arreglo_mates = [] //arreglo de mates
     arreglo_mates.push(new Mate(1, "Mate acero inox.", 10000, "Stanley", "Rojo"))
     arreglo_mates.push(new Mate(2, "Mate acero inox.", 10000, "Stanley", "Verde"))
     arreglo_mates.push(new Mate(3, "Mate acero inox.", 10000, "Stanley", "Blanco"))
@@ -65,7 +65,7 @@ let arreglo_mates = []
     arreglo_mates.push(new Mate(9, "Mate calabaza", 8000, "Particular", "Negro"))
     arreglo_mates.push(new Mate(10, "Mate calabaza", 8000, "Particular", "Rojizo"))
 
-let arreglo_vasos = []
+let arreglo_vasos = [] //arreglo de vasos
     arreglo_vasos.push(new Vaso(1, "Vaso térmico", 20000, "Stanley", "Naranja"))
     arreglo_vasos.push(new Vaso(2, "Vaso térmico", 20000, "Stanley", "Rojo"))
     arreglo_vasos.push(new Vaso(3, "Vaso térmico", 20000, "Stanley", "Blanco"))
@@ -73,7 +73,7 @@ let arreglo_vasos = []
     arreglo_vasos.push(new Vaso(5, "Vaso acero inox.", 15000, "Otro", "Metal"))
     arreglo_vasos.push(new Vaso(6, "Vaso acero inox.", 15000, "Otro", "Rojo"))
 
-let arreglo_termos = []
+let arreglo_termos = [] //arreglo de termos
     arreglo_termos.push(new Termo(1, "Termo con manija", 25000, "Stanley", "Negro"))
     arreglo_termos.push(new Termo(2, "Termo con manija", 25000, "Stanley", "Blanco"))
     arreglo_termos.push(new Termo(3, "Termo con manija", 25000, "Stanley", "Rosa"))
@@ -83,50 +83,60 @@ let arreglo_termos = []
     arreglo_termos.push(new Termo(7, "Termo sin manija", 23000, "Stanley", "Azul"))
 
 let respuesta = true
+let suma = 0
+let productos = ""
 
-while (respuesta === true){
+while (respuesta !== false){
     respuesta = mostrar_menu()
 }
-suma = 0
-let productos = ""
+
 function mostrar_menu(){
-let opcion = prompt("Que accion desea realizar ? \n 1) Ver catalogo MATES \n 2) Ver catalogo VASOS \n 3) Ver catalogo TERMOS \n 4) Salir");
+
+    let opcion = prompt("Que accion desea realizar ? \n 1) Ver catalogo MATES \n 2) Ver catalogo VASOS \n 3) Ver catalogo TERMOS \n 4) Salir");
 
 
     
         if (opcion == "1")
-            {
+        {
                 
             alert("Los mates son:\n" + mostrar_catalogo_mates());
-            confirm ("\n Desea sumar algun producto al carrito?")
-            
-            while(confirm("Desea sumar algun producto al carrito?")){
-                let model = parseInt(prompt("Indique numero de modelo")) -1
-                if(model >= 0 && model < arreglo_mates.length){
-                            suma += arreglo_mates[model].getPrecio();
-                            productos += "\n" + arreglo_mates[model].mostrar_catalogo_mates();
-                            } else {
-                            alert("Número de modelo inválido.");
-                        }
+            while(confirm("¿Desea sumar algún producto al carrito?")) {
+
+                let model = parseInt(prompt("Indique número de modelo :\n" + mostrar_catalogo_mates())) - 1; // -1 porque los arrays empiezan en 0
+                if(model >= 0 && model < arreglo_mates.length) {
+
+                    suma += arreglo_mates[model].getPrecio();
+                    productos += "\n" + arreglo_mates[model].mostrar_catalogo_mates();
+
+                 }
+
+                else {
+
+                    alert("Número de modelo inválido.");
+
+                    }
+}
 
             }
-            }
-
 
             if (opcion == "2")
-            {
+            {   
 
             alert("Los vasos son:\n" + mostrar_catalogo_vasos());
-            confirm ("\n Desea sumar algun producto al carrito?")
+
             
             while(confirm("Desea sumar algun producto al carrito?")){
-                let model = parseInt(prompt("Indique numero de modelo")) -1
-                if(model >= 0 && model < arreglo_mates.length){
-                            suma += arreglo_mates[model].getPrecio();
-                            productos += "\n" + arreglo_mates[model].mostrar_catalogo_mates();
-                            } else {
+                let model = parseInt(prompt("Indique numero de modelo"+ mostrar_catalogo_vasos())) -1
+                if(model >= 0 && model < arreglo_vasos.length){
+
+                        suma += arreglo_vasos[model].getPrecio();
+                        productos += "\n" + arreglo_vasos[model].mostrar_catalogo_vasos();
+                        } else {
+
                             alert("Número de modelo inválido.");
+
                         }
+
 
 
             }
@@ -135,39 +145,46 @@ let opcion = prompt("Que accion desea realizar ? \n 1) Ver catalogo MATES \n 2) 
 
             if (opcion == "3")
             {
-            alert("Los termos son:\n" + mostrar_catalogo_termos());
-            confirm ("\n Desea sumar algun producto al carrito?")
+
+                alert("Los termos son:\n" + mostrar_catalogo_termos());
+ 
             
-            while(confirm("Desea sumar algun producto al carrito?")){
-                let model = parseInt(prompt("Indique numero de modelo")) -1
-                if(model >= 0 && model < arreglo_mates.length){
-                            suma += arreglo_mates[model].getPrecio();
-                            productos += "\n" + arreglo_mates[model].mostrar_catalogo_mates();
-                            } else {
-                            alert("Número de modelo inválido.");
+                while(confirm("Desea sumar algun producto al carrito?")){
+
+                    let model = parseInt(prompt("Indique numero de modelo"+ mostrar_catalogo_termos())) -1
+                    if(model >= 0 && model < arreglo_termos.length){
+
+                        suma += arreglo_termos[model].getPrecio();
+                        productos += "\n" + arreglo_termos[model].mostrar_catalogo_termos();
+
+                    } else {
+
+                        alert("Número de modelo inválido.");
+
                         }
 
 
             }
             }
 
-            else if(opcion === "4") {
-            alert("Gracias por visitarnos, el total de su compra es: $" + suma + "\n Y los productos son: " + productos)
-            return false; // corta el while principal
+            else if(opcion == "4") 
+            {
 
-             }
-
-             return true // vuelve a mostrar el menú
+                alert("Gracias por visitarnos, el total de su compra es: $" + suma + "\n Y los productos son: " + productos)
+                return false; // corta el while principal
 
             }
+
+            return true
+
+            }
+
+
+            
+
             
             
-
-        
             
-
-
-
 
 function mostrar_catalogo_mates(){
     let stock_m = ""
